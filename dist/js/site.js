6c37e5ae86fc6992b158f592e55ac7817d432f18
Anton Grakhov <ant.grakhov@gmail.com>
2019-08-05 22:29:56 +0300

    Next update

--- dist/js/site.js
+++ dist/js/site.js
@@ -1,4 +1,35 @@
 (function($){
+	var body = $('body')
+
+
+	/**
+	 * Mobile menu
+	 */
+
+	var mobileMenu = $('.mobile-main-menu')
+
+	var mobileMenuToggle = $('.header-menu-toggle')
+		.on('menu.show', function(){
+			body.addClass('is-mobile-menu-show')
+		})
+		.on('menu.hide', function(){
+			body.removeClass('is-mobile-menu-show')
+		})
+		.on('click', function(){
+			mobileMenuToggle.trigger('menu.show')
+		})
+
+	mobileMenu.parent().after('<div class="mobile-menu-close" title="Закрыть меню"></div>')
+
+	$('.mobile-menu-close').on('click', function(){
+		mobileMenuToggle.trigger('menu.hide')
+	})
+
+	$('.mobile-menu-overlay').on('click', function(){
+		mobileMenuToggle.trigger('menu.hide')
+	})
+
+
 	/**
 	 * Owl Carousel
 	 */
